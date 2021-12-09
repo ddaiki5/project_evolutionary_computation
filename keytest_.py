a41efc6c58b1413d4ea91838592c9044e1cfe44f
fiftyhundred <pokemas2525@gmail.com>
2021-12-09 22:36:53 +0900

    keytest_.pyとgenetic_select.pyにコメントアウト追加、genetic_select.pyの修正

--- keytest_.py
+++ keytest_.py
@@ -10,16 +10,30 @@ KeyoutputList クラス
 
 関数
 ・read_keylist(self,filename)
+    Parameters
+    ----------
+    filename : str
+        txtファイルのファイル名
+
     filenameで指定したtxtファイルからキー出力のパターンを読み込み、keylistに代入する
     txtファイルの書き方はtest.txt参照
+
+・read_gene(self, gene)
+    Parameters
+    ----------
+    gene : np.ndarray
+        染色体配列(1次元配列)
+
+    geneで指定した染色体の遺伝子(0～20の整数)を読み込み、キー出力のパターンに変換し、keylistに代入する
 
 ・print_keylist(self)
     keylistの中身を出力する
 
-・output(self)
+・output(self, t)
     キー出力をkeylistに従って行う
+    t秒経過するか、keylistを全てキー出力し終えたら関数を終了する
 
-
+キー出力パターンと数字の対応
 0,1,2:noneの0.1,0.5,0.8
 3,4,5:Qの0.1,0.5,0.8
 6,7,8:Wの...
@@ -29,8 +43,8 @@ KeyoutputList クラス
 18,19,20:W+O
 """
 
-SECLIST = (0.1, 0.5, 0.8)
-WORDLIST = (['none'], ['q'], ['w'], ['o'], ['p'], ['q','p'], ['w','o'])
+SECLIST = (0.1, 0.5, 0.8)#秒数のリスト、read_geneで使用
+WORDLIST = (['none'], ['q'], ['w'], ['o'], ['p'], ['q','p'], ['w','o'])#キーのリスト、read_geneで使用
 
 class KeyOutputList(object):
     def __init__(self):
@@ -47,7 +61,7 @@ class KeyOutputList(object):
     def read_gene(self, gene):
         for genome in gene:
             newkeylist = list()
-            newkeylist.extend(WORDLIST[genome//3])
+            newkeylist.extend(WORDLIST[genome//3])#数字の剰余と商からキー出力を求める
             newkeylist.append(SECLIST[genome%3])
             self.keylist.append(newkeylist)
 
@@ -59,15 +73,15 @@ class KeyOutputList(object):
         for keydata in self.keylist:
             sec = keydata.pop(-1)
             for key in keydata:
-                pyautogui.keyDown(key)
+                pyautogui.keyDown(key)#キーを押す
             time.sleep(sec)
             for key in keydata:
-                pyautogui.keyUp(key)
+                pyautogui.keyUp(key)#キーを離す
             t2 = time.time()
-            if(t2-t1>t):
+            if(t2-t1>t):#指定時間を越えたら終了
                 break
 
-
+#以下テスト用
 # time.sleep(2)
 # KOLtest = KeyOutputList()
 # chromo = np.random.randint(0, 21, (5, 10))
